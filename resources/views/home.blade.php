aed537575fdc5ba1f774262c4cb27fe5e6163002
giomen88 <giorgia9288@virgilio.it>
2022-09-20 14:43:10 +0200

    modifiche css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,15 @@
 @section('title', 'Home')
 
 @section('main-content')
+<section id="movies" class="py-5">
 <div class="container d-flex flex-wrap text-center">
 @forelse($movies as $movie)
 <div class="movie card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="...">
+     <img src="" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">{{ $movie->title }}</h5>
-      <p class="card-text">{{ $movie->date }}</p>
+      <p class="card-date">{{ $movie->date }}</p>
+      <p class="card-nationality">{{ $movie->nationality }}</p>
       <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
   </div>
@@ -17,4 +19,5 @@
   <h3>Nessun film disponibile</h3>
 @endforelse 
 </div>
+</section>
 @endsection
